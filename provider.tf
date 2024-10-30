e65f6d7171e5eea9739c3f5caf0326bfd36ee8b8
Nishad89 <78018761+Nishad89@users.noreply.github.com>
2024-10-30 16:49:59 +0530

    Add files via upload

---
+++ provider.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "my-backet123"
+    key     = "terraform/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
---
+++ qa.tfvars
@@ -0,0 +1,3 @@
+ami            = "ami-0866a3c8686eaeeba"
+instance_type  = "t2.medium"
+s3_bucket_name = "teraform-bucket-7654"
