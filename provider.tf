e06c42b7defb2b41e6c3503b29c6372f80842a77
Nishad <nnishad89@gmail.com>
2024-11-13 18:37:21 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,10 +1,3 @@
 provider "aws" {
   region = "us-east-1"
 }
-terraform {
-  backend "s3" {
-    bucket         = "my-backet123"
-    key            = "terraform/dev/test.tfstate"
-    region         = "us-east-1"
-  }
-}
