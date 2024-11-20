f814ed77f3cb05248112e922ba0ca66f2a7dc4fd
Nishad <nnishad89@gmail.com>
2024-11-20 16:17:35 +0530

    initial commit

--- main.tf
+++ main.tf
@@ -39,17 +39,34 @@ resource "aws_instance" "jenkins_server" {
   # Script to install Jenkins
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update
+              sudo apt update -y
+              sudo apt install -y curl unzip
+              curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+              unzip awscliv2.zip
+              sudo ./aws/install
+              sudo apt update -y
               sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-              https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
               echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
               https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
               /etc/apt/sources.list.d/jenkins.list > /dev/null
-              sudo apt-get update
-              sudo apt-get install fontconfig openjdk-17-jre
-              sudo apt-get install jenkins
+              sudo apt-get update -y
+              sudo apt-get install -y fontconfig openjdk-17-jre
+              sudo apt-get install -y jenkins
               sudo systemctl start jenkins
-              sudo systemctl enable jenkins
+              sudo apt-get update && sudo apt-get install -y gnupg software-properties-common
+              wget -O- https://apt.releases.hashicorp.com/gpg | \
+              gpg --dearmor | \
+              sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+              gpg --no-default-keyring \
+              --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+              --fingerprint
+
+              echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+              https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+              sudo tee /etc/apt/sources.list.d/hashicorp.list
+
+              sudo apt update -y
+              sudo apt-get install -y terraform
               EOF
   tags = {
     Name = "Jenkins-Server"
