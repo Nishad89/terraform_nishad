8203fa5421cee3bbac4d8f60c54d77fc808dd59d
Nishad <nnishad89@gmail.com>
2024-11-25 13:50:42 +0530

    Update main.tf

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -53,3 +53,55 @@ resource "aws_eks_cluster" "eks_cluster" {
 
   depends_on = [aws_iam_role_policy_attachment.eks_policy_attachment]
 }
+
+# IAM Role for EKS Worker Node Group
+resource "aws_iam_role" "eks_node_role" {
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "eks-node-role"
+  }
+}
+
+# Attach the necessary policies to the EKS node role
+resource "aws_iam_role_policy_attachment" "eks_node_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_vpc_cni_policy_attachment" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
+}
+
+# EKS Node Group Configuration
+resource "aws_eks_node_group" "eks_node_group" {
+  cluster_name    = aws_eks_cluster.eks_cluster.name
+  node_group_name = "my-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = data.aws_subnet_ids.eks_subnets.ids
+  instance_types  = ["t3.small"]  # Modify based on your requirements
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  depends_on = [aws_eks_cluster.eks_cluster]
+}
