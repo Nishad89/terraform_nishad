775ecdf67516c366e4a6a735d4a06aed2ded122c
Nishad <nnishad89@gmail.com>
2024-11-25 15:26:16 +0530

    Update main.tf

--- module/eks/main.tf
+++ module/eks/main.tf
@@ -98,7 +98,7 @@ resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "my-node-group"
   node_role_arn   = aws_iam_role.eks_node_role.arn
-  subnet_ids      = data.aws_subnet_ids.eks_subnets.ids
+  subnet_ids      = data.aws_subnets.eks_subnets.ids
   instance_types  = ["t3.small"]  # Modify based on your requirements
   scaling_config {
     desired_size = 2
